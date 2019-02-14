c89f6c21aae8d0b6e25698e13569c43c0d8a804a
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-02-14 17:54:26 +0000

    Automatically backed up by Learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -22,9 +22,7 @@ end
 def list(my_songs)
   #this method is different! Collect the keys of the my_songs hash and 
   #list the songs by name
-  my_songs.each_with_index do |(k, v), index|
-    puts "#{index + 1}. #{k}"
-  end
+
 end
 
 
@@ -36,22 +34,12 @@ def play(my_songs)
   #if it isn't, tell them their choice is invalid
   #if it is, play the song using the system 'open <file path>' syntax
   #get the file path of the song by looking it up in the my_songs hash
-  puts "Please enter a song name:" 
-  song_choice = gets.chomp 
-  my_songs.each do |song_name, path|
-    if song_choice == song_name
-      puts "Playing #{song_name}"
-      system "open #{path}"
-    else
-      puts "Invalid input, please try again"
-    end
-  end
-  
+
 end
 
 def exit_jukebox
   #this method is the same as in jukebox.rb
-  puts "Goodbye"
+
 end
 
 def run(my_songs)
