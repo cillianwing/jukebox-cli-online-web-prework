fe1858798cf0ed58b318cc5db74f8a9c2268c2cf
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-02-14 19:04:09 +0000

    Automatically backed up by Learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -43,11 +43,12 @@ def play(my_songs)
   
   puts "Please enter a song name:"
   input = gets.chomp
-  my_songs.each do |song, location|
-    if input == song 
-      system "open #{location}"
+  if my_songs.has_key?(input)
+    my_songs.each do |song, location|
+      if input == song 
+        system "open #{location}"
+      end
     end
-  end
   
 end
 
